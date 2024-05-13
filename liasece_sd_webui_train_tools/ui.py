ab969e7dd9ff6e2073c63cb0c422c33cb7e622c4
w2starry <106738697+w2starry@users.noreply.github.com>
2024-05-13 11:10:38 +0800

    Update ui.py

--- liasece_sd_webui_train_tools/ui.py
+++ liasece_sd_webui_train_tools/ui.py
@@ -142,8 +142,8 @@ def new_ui():
                         train_num_epochs = gr.Number(value=40, label="Number of epochs", elem_id="train_num_epochs", interactive = True)
                         train_learning_rate = gr.Textbox(value="0.0001", label="Learning rate(Multi-select e.g. 0.0001,0.0002)", elem_id="train_learning_rate", interactive = True)
 
-                        train_scheduler = gr.Dropdown(["linear", "cosine", "cosine_with_restarts"], value="cosine", label="lr_scheduler", interactive=True)
-                        train_cosine_restarts = gr.Textbox(value=None, label="the number of times lr restarts", placeholder="(Optional)For Cosine with restart only", lines=1, max_lines=1, interactive=True)
+                        train_scheduler = gr.Dropdown(["linear", "cosine", "cosine_with_restarts"], value="cosine", label="lr_scheduler", elem_id="train_scheduler", interactive=True)
+                        train_cosine_restarts = gr.Textbox(value=None, label="the number of times lr restarts", placeholder="(Optional)For Cosine with restart only", elem_id="train_cosine_restarts", lines=1, max_lines=1, interactive=True)
 
                         sd_script_args = gr.Textbox(value="", label="Append or override the sd_script args. (e.g. `--lr_scheduler=\"constant_with_warmup\" --max_grad_norm=0.0`)", elem_id="sd_script_args", interactive = True)
                     with gr.Column():
