46e97b21cb642a856622d8b18f9e6a193f59bfbc
w2starry <106738697+w2starry@users.noreply.github.com>
2024-05-17 00:11:16 +0800

    Update ui.py

--- liasece_sd_webui_train_tools/ui.py
+++ liasece_sd_webui_train_tools/ui.py
@@ -16,9 +16,6 @@ from liasece_sd_webui_train_tools.dateset_ui import *
 from liasece_sd_webui_train_tools.train_ui import *
 import liasece_sd_webui_train_tools.sd_scripts.train_network as train_network
 
-# Setup command executor
-executor = CommandExecutor()
-
 def new_ui():
     # ====UI====
     # prepare for train data set
